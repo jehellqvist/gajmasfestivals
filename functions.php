3a6d2e01ddfb2b24a30ba7eebcca6f50acd9ca34
Jennie Hellqvist <je.hellqvist@gmail.com>
2015-04-20 15:30:57 +0200

    update bootstrap

--- functions.php
+++ functions.php
@@ -1,11 +1,21 @@
 <?php
+
+if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
+function my_jquery_enqueue() {
+   wp_deregister_script('jquery');
+   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js", false, null);
+   wp_enqueue_script('jquery');
+}
+
     function query_styles() {
-    wp_enqueue_style('gajmas-styles',  get_stylesheet_directory_uri() . '/css/style.css');
+    wp_enqueue_style('gajmas-styles',  get_stylesheet_directory_uri() . '/style.css');
     wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css' );
     wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap-responsive.min.css' );
-    wp_enqueue_script( 'bootstrap-script', get_template_directory_uri() . '/js/bootstrap.min.js', array(), true );
+    wp_enqueue_script( 'bootstrap-script', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), true );
 }
     add_action( 'wp_enqueue_scripts', 'query_styles' );
+
+
 
 /**
  * html5_shiv function.
--- header.php
+++ header.php
@@ -2,9 +2,10 @@
 <html <?php language_attributes() ?>>
 <head>
 <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 <meta name="description" value="<?php bloginfo('description'); ?>">
- 
+
 <?php wp_head(); ?>
  
 </head>
@@ -13,13 +14,26 @@
      
     <div id="wrapper">
      
-    <header>
-        <nav>               
-            <?php 
+    <header role="banner">
+
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <?php 
             //get all pages and create navigation
             $pagemenu = get_pages( array( 'sort_column' => 'menu_order', 'sort_order' => 'asc', 'parent'=> '0') ); 
 
-            echo"<ul id='nav' class='nav menu'>";
+            echo"<ul id='nav' class='nav navbar-nav'>";
                 foreach ($pagemenu as $pagemenu_data) {
                     $caption = $pagemenu_data->post_title;
                     $pageslug = $pagemenu_data->post_name;
@@ -28,5 +42,8 @@
             }
             echo"</ul>";
         ?>
-        </nav>
+        </div><!--/.nav-collapse -->
+      </div><!--End .container-->
+    </nav>
+
     </header>
