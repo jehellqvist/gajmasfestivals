7cce0844ac3eb3134b6896baa35c716cd19f1bcd
Jennie Hellqvist <je.hellqvist@gmail.com>
2015-04-20 14:48:21 +0200

    include bootstrap

--- functions.php
+++ functions.php
@@ -1,5 +1,27 @@
 <?php
     function query_styles() {
-    wp_enqueue_style('my-script',  get_stylesheet_directory_uri() . '/style.css');
+    wp_enqueue_style('gajmas-styles',  get_stylesheet_directory_uri() . '/css/style.css');
+    wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap-responsive.min.css' );
+    wp_enqueue_script( 'bootstrap-script', get_template_directory_uri() . '/js/bootstrap.min.js', array(), true );
 }
     add_action( 'wp_enqueue_scripts', 'query_styles' );
+
+/**
+ * html5_shiv function.
+ * 
+ * @access public
+ * @return void
+ */
+
+function html5_shiv() {
+        ?>
+        <!-- IE Fix for HTML5 Tags -->
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <?php
+}
+if(!is_admin()) {
+        add_action('wp_head','html5_shiv');
+}
