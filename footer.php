27c0ff5c92675a78ffaa7b16b18e780a011f0bfe
Elle <michelle.pantorp@bredband.net>
2015-04-23 13:57:49 +0200

    Some styling on footer

--- footer.php
+++ footer.php
@@ -1,6 +1,26 @@
 <footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-5 col-lg-offset-1">
+				<h1>Kontakta oss På Limhamn </h1>
+				<p>E-post: <a href="mailto:info@polimhamn.se?Subject=Medverkande">info@polimhamn.se </a></p>
+				<a href="http://www.polimhamn.se"><div id="logo_polimhamn"></div></a>
+				
+				<h1>Intresserad av försäljning eller marknadsstånd? </h1>
+				<p>Klicka <a href="">här</a> för mer information</p>
+
+				<h2>Hamnfestivalen pågår mellan XX juli och XX augusti.</h2>
+
+			</div>
+			<div class="col-lg-5 col-lg-offset-1">
+				<p>tetstetstestts</p>
+
+			</div>
+
+		</div>
+	</div>
+
    	<?php the_time('Y'); ?> &copy; <?php bloginfo('name'); ?><br />
-    <a href="<?php bloginfo('rss2_url'); ?>">Grab the feed</a></p>
          
 </footer>
  
