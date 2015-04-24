11fc8908e74bfabecf4793f248fb58463e6e7cc6
Jennie Hellqvist <je.hellqvist@gmail.com>
2015-04-24 10:39:27 +0200

    css update

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<meta name="description" value="<?php bloginfo('description'); ?>">
+<meta name="description" content="<?php bloginfo('description'); ?>">
 
 <?php wp_head(); ?>
  
