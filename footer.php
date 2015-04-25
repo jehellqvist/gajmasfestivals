9be6cd13863adfd9bf2724f1d4458272d4617c6b
Elle <michelle.pantorp@bredband.net>
2015-04-25 18:48:35 +0200

    Small changes

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
-<footer role="footer">
+<footer>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-3 col-lg-offset-1 center">
+			<div class="col-md-3 col-sm-3 col-md-offset-1 col-sm-offset-1 center">
 				<a href="http://www.polimhamn.se" target="_blank"><div id="polimhamn_logo"></div></a><br>
 				<p>E-post: <a href="mailto:info@polimhamn.se">info@polimhamn.se </a></p>
 				
@@ -9,17 +9,18 @@
 				<p>Klicka <a href="">här</a> för mer information</p>
 			</div><!--End of .col-->
 
-			<div class="col-md-4 center top">
+			<div class="col-md-4 col-sm-4 center top">
 				<h1>Följ Hamnfestivalen</h1>
 				<p>
 					<ul class="list-unstyled list-inline">
-						<li><a class="fa fa-facebook" href="https://www.facebook.com/hamnfestivalenpolimhamn" title="Facebook" target="_blank"></a></li>
-			            <li><a class="fa fa-instagram" href="https://instagram.com/pohamnfestivalen/" title="Instagram" target="_blank"></a></li>
+						<li><a class="fa fa-facebook-square" href="https://www.facebook.com/hamnfestivalenpolimhamn" title="Facebook" target="_blank"></a></li>
+						<li><a class="fa fa-instagram" href="https://instagram.com/pohamnfestivalen/" title="Instagram" target="_blank"></a></li>
+						<li><a class="fa fa-google-plus-square" href="https://plus.google.com/share?url=http://www.hamnfestivalen.se/" title="Google +" target="_blank"></a></li>
 			        </ul>
 			    </p>
 			</div><!--End of .col-->
 
-			<div class="col-md-3 center">
+			<div class="col-md-3 col-sm-3 center">
 				<h1>Meny</h1>
 				<p>Program</p>
 				<p>Hitta hit</p>
