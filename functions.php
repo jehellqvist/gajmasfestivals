058a0159878fe90dd9121ef458d73b247b824865
AndiiW <acawiig@gmail.com>
2015-04-30 10:32:42 +0200

    included custom fields
    direkt installerat i temat

--- functions.php
+++ functions.php
@@ -89,6 +89,7 @@ function posts_callback($atts=null, $content=null){
                     <p>'.get_the_post_thumbnail().'</p>
                     <p>'. get_the_content().'</p>
                     <span>Plats: '.get_field('plats').'</span><br>
+                    <span>Datum: '.get_field('datum').'</span>
                     <span>Tid: '.get_field('tid').'</span>
                 </div><!--End .inner--></div><!--End . col-*-* -->';
                 ?><?php $cat_string = "";
@@ -214,4 +215,40 @@ function posts_custom_columns($column_name, $id){
         echo the_post_thumbnail( 'thumbnail' );
     }
 }
+
+
+// 1. customize ACF path
+add_filter('/acf/settings/path', 'my_acf_settings_path');
+ 
+function my_acf_settings_path( $path ) {
+ 
+    // update path
+    $path = get_stylesheet_directory() . '/acf/';
+    
+    // return
+    return $path;
+    
+}
+ 
+
+// 2. customize ACF dir
+add_filter('/acf/settings/dir', 'my_acf_settings_dir');
+ 
+function my_acf_settings_dir( $dir ) {
+ 
+    // update path
+    $dir = get_stylesheet_directory_uri() . '/acf/';
+    
+    // return
+    return $dir;
+    
+}
+ 
+
+// 3. Hide ACF field group menu item
+add_filter('/acf/settings/show_admin', '__return_false');
+
+
+// 4. Include ACF
+include_once( get_stylesheet_directory() . '/acf/acf.php' );
 
