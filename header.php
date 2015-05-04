5f7479cdb000b4f8ea3563f57fd88df4793907f0
Jennie Hellqvist <je.hellqvist@gmail.com>
2015-05-04 09:46:12 +0200

    update

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
+<head>
+<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<meta name="description" content="<?php bloginfo('description'); ?>">
+
+<?php wp_head(); ?>
+ 
+</head>
+  
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
