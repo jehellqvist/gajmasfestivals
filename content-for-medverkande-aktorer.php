76cf859897e4637147fc2eb51b09a6a5a43bb95a
Elle <michelle.pantorp@bredband.net>
2015-05-12 09:08:35 +0200

    content for medverkande aktörer

---
+++ content-for-medverkande-aktorer.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Template Name: for-medverkande-aktorer
+*
+* //used instead of page.php is choosen as template for the page
+*/
+
+?>
+
+
+<div class="row">
+<div class="col-md-5 col-md-offset-2">
+<h1>Aktör på Hamnfestivalen?</h1>
+<h2>30 juli - 2 augusti 2015</h2>
+<img src="http://localhost/wp-content/uploads/2015/05/vimplat_bred.png" alt="vimpel sidebar" />
+
+</div>
+</div>
+<div class="row">
+<div class="col-md-7 col-md-offset-1">
+
+Hamnfestivalen bjuder på fyra festfyllda dagar med upplevelser, musik, mat &amp; kultur. I juli 2014 slog portarna upp till en fantastisk Hamnfestival som arrangerades längs havet i Limhamn. Hamnfestivalen 2014 drog över 15000 besökare och succén var omedelbar. Årets Hamnfestival går av stapeln den 30 juli och pågår t.om. den 2 augusti.
+
+Är du intresserad av att bli en medverkande aktör på Hamnfestivalen?
+<h3>Anmälan</h3>
+Blankett för anmälan till att medverka på Hamnfestivalen fylls i och skickas via e-post till:
+
+<i class="fa fa-envelope"></i><a href="mailto:info@polimhamn.se?Subject=Anmälan%20medverkande%20aktör">info@polimhamn.se </a>
+
+Tänk på att anmälan sker omgående och är bindande.
+
+Krav på medverkan samtliga dagar. Avtal kommer skrivas med samtliga aktörer. Mer praktisk information delges vid avtalsskrivningen. Bodar och marknadsstånd finns på plats till ert förfogande. Bodarna är till för våra mataktörer och finns i begränsat antal. Övriga aktörer har tillgång till moderna marknadsstånd som har en exponeringsyta på 220x80cm.
+<h3>Plats, Öppetider &amp; Pris</h3>
+Årets Hamnfestival är primärt förlagd i och kring Småbåtshamnen och Fiskehamnen. Fiskehamnen håller öppet för besökare mellan kl 11.00 - 20.00 alla dagar och Småbåtshamnen mellan kl. 11.00 - 22.00 alla dagar.
+<table>
+<tbody>
+<tr>
+<th>Marknadsstånd</th>
+<th>Samtliga dagar</th>
+</tr>
+<tr>
+<td>Matförsäljning</td>
+<td>2800 kr ex. moms</td>
+</tr>
+<tr>
+<td>Konsthantverk &amp; Övrig försäljning</td>
+<td>2000 kr exkl. moms</td>
+</tr>
+</tbody>
+</table>
+</div>
+<div class="col-md-3 sidebar">
+<div class="row">
+<div class="col-md-12 col-sm-4 col-xs-12">
+<h3>Blanketter och Avtal</h3>
+<a class="left" href="http://localhost/wp-content/uploads/2015/05/Avtal_AKTIVITETER.pdf" download="avtal_aktiviteter.pdf"><i class="fa fa-download"></i>Blankett för aktiviteter </a>
+<p class="top-neg"><a class="left" href="http://localhost/wp-content/uploads/2015/05/Avtal_MAT.pdf" download="avtal_mat.pdf"><i class="fa fa-download"></i>Blankett för mat</a></p>
+<p class="top-neg"><a class="left" href="http://localhost/wp-content/uploads/2015/05/Avtal_FÖRSÄLJNING.pdf" download="avtal_forsaljning.pdf"><i class="fa fa-download"></i>Blankett för försäljning</a></p>
+<p class="top-neg"><a class="left" href="http://localhost/wp-content/uploads/2015/05/Avtal_EL.pdf" download="avtal_el.pdf"><i class="fa fa-download"></i>Blankett för el</a></p>
+
+</div>
+<div class="col-md-12 col-sm-4 col-xs-12">
+<h3>Arrangör</h3>
+<ul class="arrangor-kontakt">
+	<li>PÅ LIMHAMN ek. förening</li>
+	<li>Gitte Knutsson, Verksamhetsledare</li>
+	<li><i class="fa fa-phone"></i>0760-104405</li>
+</ul>
+</div>
+<div class="col-md-12 col-sm-4 col-xs-12">
+<h3>Arrangemanget marknadsförs i</h3>
+Sydsvenskan, Magasin Limhamn, Malmö stads hemsida, www.polimhamn.se, sociala medier, affischer och andra lokala medier, pressbearbetning samt viss marknadsföring i Danmark.
+
+</div>
+</div>
+</div>
+</div>
