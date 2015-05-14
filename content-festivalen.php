d9a92e3971a730da99a42888647e5b153b0d6402
amanda <amndaek@gmail.com>
2015-05-14 18:06:20 +0200

    styling content-festivalen

--- content-festivalen.php
+++ content-festivalen.php
@@ -8,20 +8,57 @@ Template Name: festivalen
 ?>
 
 <section id='<?php echo $post->post_name; ?>' class='container-fluid'>
-    <h1><?php the_title()?></h1>
+	<div class="row festivalen">	    
+		<h1><?php echo get_field('rubrik')?></h1>		
+		<h4><?php echo get_field('introtext')?></h4>
+		<a href="http://localhost/gajmas/for-medverkande-aktorer/" class="btn"><?php echo get_field('medverka_lank')?></a>
+	</div>
+	
 	<div class="row">
-		<h1><?php echo get_field('rubrik')?></h1>
-		<p><?php echo get_field('introtext')?></p>
-		<p><?php echo get_field('medverka_lank')?></p>
-		<p><?php echo get_field('aktor_1')?></p>
-		<img src='<?php echo get_field('aktor_1_bild')?>' alt="Bild på aktör 1">
-		<p><?php echo get_field('aktor_1_citat')?></p>
-		<p><?php echo get_field('aktor_2')?></p>
-		<img src="<?php echo get_field('aktor_2_bild')?>" alt="Bild på aktör 2">
-		<p><?php echo get_field('aktor_2_citat')?></p>
-		<p><?php echo get_field('aktor_3')?></p>
-		<img src="<?php echo get_field('aktor_3_bild')?>" alt="Bild på aktör 3">
-		<p><?php echo get_field('aktor_3_citat')?></p>
-	</div><!-- .row .entry-content -->
-
+		<article class="actor post-content col-md-4">
+			<a href="">
+				<div class="inner" style="background-image:url('<?php echo get_field('aktor_1_bild')?>'); background-position: top;"></div>
+				<div class="inner-bottom">
+					<div class="wave">
+						<div id="citat" class="inner-content">
+							<p class="description"><?php echo get_field('aktor_1_citat')?></p>
+							<div class="author">
+								<p>- <?php echo get_field('aktor_1')?></p>
+							</div>
+						</div><!--.inner-content-->
+					</div><!--.wave-->
+				</div><!--.inner-bottom-->
+			</a><!--End .inner-->
+		</article><!--End . col-*-* -->
+		<article class="actor post-content col-md-4">
+			<a href="">
+				<div class="inner" style="background-image:url('<?php echo get_field('aktor_2_bild')?>')"></div>
+				<div class="inner-bottom">
+					<div class="wave">
+						<div class="inner-content">
+							<p class="description"><?php echo get_field('aktor_2_citat')?></p>
+							<div class="author">
+								<p>- <?php echo get_field('aktor_2')?></p>
+							</div>
+						</div><!--.inner-content-->
+					</div><!--.wave-->
+				</div><!--.inner-bottom-->
+			</a><!--End .inner-->
+		</article><!--End . col-*-* -->
+		<article class="actor post-content col-md-4">
+			<a href="">
+				<div class="inner" style="background-image:url('<?php echo get_field('aktor_3_bild')?>')"></div>
+				<div class="inner-bottom">
+					<div class="wave">
+						<div class="inner-content">
+							<p class="description"><?php echo get_field('aktor_3_citat')?></p>
+							<div class="author">
+								<p>- <?php echo get_field('aktor_3')?></p>
+							</div>
+						</div><!--.inner-content-->
+					</div><!--.wave-->
+				</div><!--.inner-bottom-->
+			</a><!--End .inner-->
+		</article><!--End . col-*-* -->
+	</div>
 </section><!-- #post-## -->
---
+++ content-info.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Template Name: info
+*
+* //used instead of page.php is choosen as template for the page
+*/
+
+?>
+
+<section id='<?php echo $post->post_name; ?>' class='container-fluid'>
+	<div class="row">
+		<article class="col-md-4">
+			<div class="info">
+				<p>Info 1</p>
+			</div>
+		</article>
+		<article class="col-md-4">
+			<div class="info">
+				<p>Info 2</p>
+			</div>
+		</article>
+		<article class="col-md-4">
+			<div class="info">
+				<p>Info 3</p>
+			</div>
+		</article>
+	</div>
+</section>
