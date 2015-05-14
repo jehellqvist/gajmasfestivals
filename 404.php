20c87d4294c71b84afc3b35d69b629d2b2c11cb4
Jennie Hellqvist <je.hellqvist@gmail.com>
2015-05-14 20:05:59 +0200

    404

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ */
+ 
+get_header(); ?>
+ 
+    <section id="primary" class="content-area container-fluid not-found">
+        <div id="content" class="site-content row" role="main">
+            <div class="col-md-12 text-center">
+                    <h1 class="entry-title">404 - Man överbord!</h1>
+                    <p>Sidan du söker finns inte - simma tillbaka eller ta båten till <a href="/">startsidan.</a></p>
+            </div><!--.col-md-12-->
+ 
+        </div><!-- #content .site-content -->
+    </section><!-- #primary .content-area -->
+ 
+<?php get_footer(); ?>
