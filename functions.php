593cd19cdc0698b538cbda0233f34996fdc5c7e6
Jennie Hellqvist <je.hellqvist@gmail.com>
2015-05-21 10:09:52 +0200

    update

--- functions.php
+++ functions.php
@@ -198,20 +198,7 @@ function html5_shiv() {
 if(!is_admin()) {
         add_action('wp_head','html5_shiv');
 }
-if(get_page_by_title("Home") == null)
-{
-    $post = array(
-        "post_title" => "Home",
-        "post_status" => "publish",
-        "post_type" => "page",
-        "menu_order" => "-100",
-        "page_template" => "single-page-theme.php"
-    );
-    wp_insert_post($post);
-    $home_page = get_page_by_title("Home");
-    update_option("page_on_front",$home_page->ID);
-    update_option("show_on_front","page");
-}
+
 function posts_callback($atts=null, $content=null){
     query_posts(array('orderby' => 'date','order' => 'DESC' , 'showposts' => $posts));
             $option .='
